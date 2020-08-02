f9fd1459800d55bc03bba49ff43ff1a32a16a06b
Kaeldir <legal.mikael@gmail.com>
2020-08-02 20:40:11 +0200

    Updates

---
+++ precache-manifest.fe90f3923aa394a6add738913e7f47d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7352acc4cb7d1899066126041ef069a0",
+    "url": "/Space-Jam-Web/index.html"
+  },
+  {
+    "revision": "8e5eb592756a82f5d4fa",
+    "url": "/Space-Jam-Web/static/js/2.1fb24ece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Space-Jam-Web/static/js/2.1fb24ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3966e7668569902ef11c",
+    "url": "/Space-Jam-Web/static/js/main.1d4a66bf.chunk.js"
+  },
+  {
+    "revision": "b6c61b7b2ad08c974f53",
+    "url": "/Space-Jam-Web/static/js/runtime-main.662b37cb.js"
+  }
+]);
